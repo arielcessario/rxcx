cd2855676526bc9954544e3eb5927bf60d1d2b30
Mateo Maneff <mmaneff@gmail.com>
2019-08-07 23:22:36 -0300

    Correcciones

--- src/app/admin/definicion/definicion.component.ts
+++ src/app/admin/definicion/definicion.component.ts
@@ -1,13 +1,13 @@
 import { RxCxProxy } from 'rxcx-core';
-import { Validators } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { FormGroup } from '@angular/forms';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { Logger } from './../../core/logger/logger.service';
 import { CoreService } from 'ac-core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Location } from '@angular/common';
 import { environment } from 'src/environments/environment';
+import { Definicion } from '../../shared/model/definicion';
+
 
 @Component({
   selector: 'rxc-definicion',
@@ -15,8 +15,9 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./definicion.component.scss']
 })
 export class DefinicionComponent implements OnInit, OnDestroy {
-  form: FormGroup;
-  private fb: FormBuilder;
+
+  definicion2: Definicion = new Definicion();
+  imagesPath = environment.imagesPath;
 
   // Nombres de las Imagenes
   _img01Name = 'no_image.png';
@@ -30,40 +31,10 @@ export class DefinicionComponent implements OnInit, OnDestroy {
 
   images = [];
 
-  imagesPath = environment.imagesPath;
-
   id = '0';
   _id = '0';
-
-  titulo = '';
-  descripcion = '';
-
   subs: any;
 
-  _definicionGroup: any = {
-    titulo: [this.titulo, [Validators.required, Validators.minLength(4)]],
-    descripcion: [
-      this.descripcion,
-      [Validators.required, Validators.minLength(4)]
-    ]
-  };
-
-  definicion: any = { uid: '0', titulo: '', descripcion: '', imagenes: [] };
-  formErrors = {
-    titulo: '',
-    descripcion: ''
-  };
-  validationMessages = {
-    titulo: {
-      required: 'Requerido',
-      minlength: 'Mínimo 3 letras',
-      maxlength: 'El nombre no puede tener mas de 24 letras'
-    },
-    descripcion: {
-      required: 'Power is required.',
-      maxlength: 'Sismbolo tiene que tener un máximo de 3 letras'
-    }
-  };
   constructor(
     private location: Location,
     private router: Router,
@@ -79,11 +50,11 @@ export class DefinicionComponent implements OnInit, OnDestroy {
       if (this.id !== '0') {
         this._id = this.id;
         this.rxcxProxy.getDefinicion(this.id).subscribe(data => {
-          this.definicion = data[0];
-          this.definicion.titulo = decodeURIComponent(data[0].titulo);
-          this.definicion.descripcion = decodeURIComponent(data[0].descripcion);
+          this.definicion2.definicion_id = data[0].definicion_id;
+          this.definicion2.titulo = data[0].titulo;
+          this.definicion2.descripcion = data[0].descripcion;
 
-          this.form = this.buildForm(this.form, this._definicionGroup);
+          // this.form = this.buildForm(this.form, this._definicionGroup);
           this.rxcxProxy.getDefinicionImagenes(this.id).subscribe(imagenes => {
             this.images = [];
             for (let i = 0; i < imagenes.length; i++) {
@@ -91,18 +62,11 @@ export class DefinicionComponent implements OnInit, OnDestroy {
             }
           });
         });
-      } else {
-        this.form = this.buildForm(this.form, this._definicionGroup);
       }
     });
   }
 
   save() {
-    delete this.definicion.uid;
-
-    this.definicion.titulo = encodeURIComponent(this.form.get('titulo').value);
-    this.definicion.descripcion = encodeURIComponent(this.form.get('descripcion').value);
-    // console.log(this.definicion);
     if (this._id === '0') {
       this.create();
     } else {
@@ -111,8 +75,9 @@ export class DefinicionComponent implements OnInit, OnDestroy {
   }
 
   create() {
-    this.definicion.imagenes = this.images;
-    this.rxcxProxy.createDefinicion(this.definicion).subscribe(response => {
+    this.definicion2.imagenes = this.images;
+
+    this.rxcxProxy.createDefinicion(this.definicion2).subscribe(response => {
       this.coreService.setToast({
         type: 'success',
         title: 'Creación',
@@ -123,11 +88,9 @@ export class DefinicionComponent implements OnInit, OnDestroy {
   }
 
   update() {
-    this.definicion.imagenes = this.images;
+    this.definicion2.imagenes = this.images;
 
-    console.log(this.definicion);
-
-    this.rxcxProxy.updateDefinicion(this.definicion).subscribe(response => {
+    this.rxcxProxy.updateDefinicion(this.definicion2).subscribe(response => {
       this.coreService.setToast({
         type: 'success',
         title: 'Modificación',
@@ -137,30 +100,9 @@ export class DefinicionComponent implements OnInit, OnDestroy {
   }
 
   updateDefinicionImages(e, id) {
-    const _id = 'img' + id;
-    this.definicion[_id] = e[_id];
-    console.log(this.definicion);
-  }
-
-  buildForm(form: FormGroup, group: any): FormGroup {
-    this.fb = new FormBuilder();
-    form = this.fb.group(group);
-    form.valueChanges.subscribe(data =>
-      this.coreService.onValueChanged(
-        data,
-        form,
-        this.formErrors,
-        this.validationMessages
-      )
-    );
-    this.coreService.onValueChanged();
-    console.log(this.definicion);
-    form.setValue({
-      titulo: this.definicion.titulo || '',
-      descripcion: this.definicion.descripcion || ''
-    });
-
-    return form;
+    // const _id = 'img' + id;
+    // this.definicion[_id] = e[_id];
+    console.log(this.definicion2);
   }
 
   volver() {
@@ -168,15 +110,15 @@ export class DefinicionComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.form = null;
+    // this.form = null;
     this._id = '';
     this.id = '';
   }
 
   loadedImage(e, index) {
-    console.log(e);
+    // console.log(e);
     this.images[index] = e.originalName;
-    console.log(this.images);
+    // console.log(this.images);
   }
 
   setImageName(_obj, val) {
